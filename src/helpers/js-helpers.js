762cfdae928814296bf333d02f75e14d7706149a
Dietrich Rordorf <dr@ediqo.com>
2018-07-20 15:23:17 +0200

    add forEach helper

---
+++ src/helpers/js-helpers.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param items array of items to loop
+ * @param callback function that takes individual items as argument
+ * @returns {Array}
+ */
+const forEach = (items, callback) => {
+    let res = [];
+    for(let i = 0; i < items.length; i++) {
+        let item = items[i];
+        res.push(callback(item));
+    }
+
+    return res;
+};
+
+
+export { forEach };
--- src/js-utils.js
+++ src/js-utils.js
@@ -5,36 +5,8 @@
  *
  */
 
-/**
- * Fade out DOM element
- * @param el DOM element
- */
-const fadeOut = (el) => {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= 0.1) < 0) {
-            el.style.display = 'none';
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+import { forEach } from './helpers/js-helpers';
+import { fadeIn, fadeOut } from "./animations/js-animations";
 
-/**
- * Fade in DOM element
- * @param el DOM element
- * @param display CSS display property, defaults tp 'block'
- */
-const fadeIn =  (el, display) => {
-    el.style.opacity = 0;
-    el.style.display = display || 'block';
-    (function fade() {
-        let val = parseFloat(el.style.opacity);
-        if (!((val += 0.1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
 
-export {fadeOut, fadeIn};
+export {fadeOut, fadeIn, forEach};
